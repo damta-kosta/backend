8d88c6df7601b874fcef66ce11c113989adcea40
thusja <wjdxo9292@naver.com>
2025-07-08 09:20:39 +0900

    update: set timezone

--- db/index.js
+++ db/index.js
@@ -7,6 +7,13 @@ const db = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWD,
     port: Number(process.env.DB_PORT)
-})
+});
+
+// 연결될 때마다 타임존 설정
+db.on('connect', (client) => {
+    client.query("SET TIME ZONE 'Asia/Seoul'")
+    .then(() => console.log("Timezone set to Asia/Seoul"))
+    .catch(err => console.error("Failed to set timezone", err));
+});
 
 module.exports = db;
