4690ebfda20224c2dad76e0a59bc82fb6ca2e50a
thusja <wjdxo9292@naver.com>
2025-07-11 10:44:10 +0900

    update: payload 1mb

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
